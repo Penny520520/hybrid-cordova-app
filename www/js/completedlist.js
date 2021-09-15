89e3b7f777fbcc195c4689b0c075f546b3e9c812
Penny520520 <82118825+Penny520520@users.noreply.github.com>
2021-09-15 01:42:19 -0700

    upload

---
+++ www/js/completedlist.js
@@ -0,0 +1,17 @@
+// completed book list data
+var completedList = [
+    {title: 'Cactus Club', time: '16:40 to 16:50 pm', num: '8'},
+    {title: 'Cactus 1', time: '16:50 to 17:00 pm', num: '1'},
+    {title: 'Cactus 2', time: '17:00 to 17:10 pm', num: '2'},
+    {title: 'Cactus 3', time: '16:40 to 16:50 pm', num: '3'},
+    {title: 'Cactus 4', time: '16:50 to 17:00 pm', num: '4'},
+    {title: 'Cactus 5', time: '17:00 to 17:10 pm', num: '5'},
+    {title: 'Cactus 6', time: '16:40 to 16:50 pm', num: '6'},
+    {title: 'Cactus 7', time: '16:50 to 17:00 pm', num: '7'},
+    {title: 'Cactus 8', time: '17:00 to 17:10 pm', num: '8'},
+    {title: 'Cactus 9', time: '16:40 to 16:50 pm', num: '8'},
+    {title: 'Cactus 10', time: '16:50 to 17:00 pm', num: '10'},
+    {title: 'Cactus 11', time: '17:00 to 17:10 pm', num: '11'},
+    {title: 'Cactus 12', time: '16:40 to 16:50 pm', num: '12'},
+    {title: 'Cactus 13', time: '16:50 to 17:00 pm', num: '13'},    
+];
